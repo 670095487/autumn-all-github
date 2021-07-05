4e76b06500f0c39c1b6274c2a4d46a7765f62eb9
yunN <670095487@qq.com>
2021-07-05 21:08:18 +0800

    Add solution for reduce.

--- autumn/src/main/java/com/yunn/autumn/someproduction/handler/TestForWork.java
+++ autumn/src/main/java/com/yunn/autumn/someproduction/handler/TestForWork.java
@@ -3,7 +3,10 @@ package com.yunn.autumn.someproduction.handler;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -15,41 +18,45 @@ public class TestForWork {
 
     public static void main(String[] args) {
         List<TestForWorkDo> list = new ArrayList<>();
-        list.add(new TestForWorkDo("90005", "lisi1", LocalDateTime.now().minusMinutes(1)));
-        list.add(new TestForWorkDo("90005", "lisi2", LocalDateTime.now().minusMinutes(2)));
-        list.add(new TestForWorkDo("90005", "lisi3", LocalDateTime.now().minusMinutes(3)));
-        list.add(new TestForWorkDo("90005", "lisi4", LocalDateTime.now().minusMinutes(4)));
-        list.add(new TestForWorkDo("90005", "lisi5", LocalDateTime.now().minusMinutes(5)));
-        list.add(new TestForWorkDo("90006", "wangwu1", LocalDateTime.now().minusMinutes(1)));
-        list.add(new TestForWorkDo("90006", "wangwu2", LocalDateTime.now().minusMinutes(2)));
-        list.add(new TestForWorkDo("90006", "wangwu3", LocalDateTime.now().minusMinutes(3)));
-        list.add(new TestForWorkDo("90006", "wangwu4", LocalDateTime.now().minusMinutes(4)));
-        list.add(new TestForWorkDo("90006", "wangwu5", LocalDateTime.now().minusMinutes(5)));
-        list.add(new TestForWorkDo("90006", "wangwu6", LocalDateTime.now().minusMinutes(6)));
-
-
-        Map<String, TestForWorkDo> result = list.stream()
-                .sorted(Comparator.comparing(TestForWorkDo::getTime).reversed())
+        Map<String, BigDecimal> resultMap = list.stream()
                 .collect(Collectors.groupingBy(TestForWorkDo::getAccount,
-                         Collectors.collectingAndThen(Collectors.toList(), value -> value.get(0))));
+                        Collectors.reducing(BigDecimal.ZERO, TestForWorkDo::getAmount, BigDecimal::add)));
+
+        Map<String, TestForWorkDo> result = getStringTestForWorkDoMap(list);
 
         List<TestForWorkDo> resultList = new ArrayList<>(result.values());
 
         System.out.println(resultList);
 
-        // System.out.println(list.stream()
-        //         .sorted(Comparator.comparing(TestForWorkDo::getTime).reversed())
-        //         .distinct()
-        //         .collect(Collectors.toList()));
+        System.out.println(YearMonth.now().minusMonths(1).toString());
+
+        System.out.println(list.stream()
+                .sorted(Comparator.comparing(TestForWorkDo::getTime).reversed())
+                .distinct()
+                .collect(Collectors.toList()));
+
+        System.out.println(YearMonth.now().getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH));
     }
+
+    private static Map<String, TestForWorkDo> getStringTestForWorkDoMap(List<TestForWorkDo> list) {
+        Map<String, TestForWorkDo> result = list.stream()
+                .sorted(Comparator.comparing(TestForWorkDo::getTime).reversed())
+                .collect(Collectors.groupingBy(TestForWorkDo::getAccount,
+                        Collectors.collectingAndThen(Collectors.toList(), value -> value.get(0))));
+        return result;
+    }
+
+
 }
 
-@AllArgsConstructor
 @Data
+@AllArgsConstructor
 class TestForWorkDo {
     private String account;
     private String name;
+    private BigDecimal amount;
     private LocalDateTime time;
+
 
     @Override
     public boolean equals(Object o) {
